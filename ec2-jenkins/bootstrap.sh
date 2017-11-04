2ec02045ab7281dc9eb04dcb6e3965cee3edcacf
Matheus Fidelis <msfidelis@outlook.com>
2017-11-04 20:33:15 -0200

    Correção setup jenkins

--- ec2-jenkins/bootstrap.sh
+++ ec2-jenkins/bootstrap.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-yum install docker docker-compose -y
+yum install docker -y
+curl -L https://github.com/docker/compose/releases/download/1.17.0/docker-compose-`uname -s`-`uname -m` -o /usr/bin/docker-compose
+chmod +x /usr/bin/docker-compose
+
+service docker start
 
 mkdir /home/jenkins && chmod 777 /home/jenkins
 

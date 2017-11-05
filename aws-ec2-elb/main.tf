bb755f19eca4052ddd2130017d087fbb82ead285
Matheus Fidelis <msfidelis@outlook.com>
2017-11-05 01:12:01 -0200

    ELB

--- aws-ec2-elb/main.tf
+++ aws-ec2-elb/main.tf
@@ -7,8 +7,8 @@ resource "aws_security_group" "default" {
   name = "ec2-elb-sg"
 
   ingress {
-    from_port = 0
-    to_port = 65535
+    from_port = 80
+    to_port = 80
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }

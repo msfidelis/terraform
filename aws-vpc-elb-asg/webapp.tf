0dcef3ca7dfa132a4cc457201e27439ad70a52c6
Matheus Fidelis <msfidelis@outlook.com>
2017-12-14 00:50:32 -0200

    policy

--- aws-vpc-elb-asg/webapp.tf
+++ aws-vpc-elb-asg/webapp.tf
@@ -146,3 +146,59 @@ resource "aws_autoscaling_group" "webapp_scalegroup" {
     }
 
 }
+
+# Auto Scaling Policy UP + 2
+resource "aws_autoscaling_policy" "autopolicy-up" {
+    name = "terraform-autoplicy"
+    scaling_adjustment = 2
+    adjustment_type = "ChangeInCapacity"
+    cooldown = 300
+    autoscaling_group_name = "${aws_autoscaling_group.webapp_scalegroup.name}"
+}
+
+# Cloud Watch Metric Alarm - CPU High
+resource "aws_cloudwatch_metric_alarm" "cpualarm" {
+    alarm_name = "terraform-alarm"
+    comparison_operator = "GreaterThanOrEqualToThreshold"
+    evaluation_periods = "2"
+    metric_name = "CPUUtilization"
+    namespace = "AWS/EC2"
+    period = "120"
+    statistic = "Average"
+    threshold = "60"
+
+    dimensions {
+        AutoScalingGroupName = "${aws_autoscaling_group.webapp_scalegroup.name}"
+    }
+
+    alarm_description = "This metric monitor EC2 instance cpu utilization"
+    alarm_actions = ["${aws_autoscaling_policy.autopolicy-up.arn}"]
+}
+
+# Auto Scaling Policy Down -1  
+resource "aws_autoscaling_policy" "autopolicy-down" {
+    name = "terraform-autoplicy-down"
+    scaling_adjustment = -1
+    adjustment_type = "ChangeInCapacity"
+    cooldown = 300
+    autoscaling_group_name = "${aws_autoscaling_group.webapp_scalegroup.name}"
+}
+
+# Cloud Watch Metric Alarm - CPU Low
+resource "aws_cloudwatch_metric_alarm" "cpualarm-down" {
+    alarm_name = "terraform-alarm-down"
+    comparison_operator = "LessThanOrEqualToThreshold"
+    evaluation_periods = "2"
+    metric_name = "CPUUtilization"
+    namespace = "AWS/EC2"
+    period = "120"
+    statistic = "Average"
+    threshold = "10"
+
+    dimensions {
+        AutoScalingGroupName = "${aws_autoscaling_group.webapp_scalegroup.name}"
+    }
+
+    alarm_description = "This metric monitor EC2 instance cpu utilization"
+    alarm_actions = ["${aws_autoscaling_policy.autopolicy-down.arn}"]
+}
